efbfbe3b1f444886b2f3b26012e78c6cd8953c4b
Darshil Trivedi <darshiltrivedi04@gmail.com>
2021-11-17 11:52:14 -0800

    Update top_layer.py

--- Phased-Array-Python/PhasedArray-python-Master/Source-code/CN-0566/top_layer.py
+++ Phased-Array-Python/PhasedArray-python-Master/Source-code/CN-0566/top_layer.py
@@ -28,16 +28,27 @@ def main():
 
     # channel 4 is the third, and channel 3 is the fourth
     # Select and connect the Beamformer/s. Each Beamformer has 4 channels
-    adar = adi.adar1000_array(
-        uri=rpi_ip,
-        chip_ids=["BEAM0", "BEAM1"],
-        device_map=[[1, 2]],
-        element_map=[[1, 2, 3, 4], [5, 6, 7, 8]],
-        device_element_map={
-            1: [2, 1, 4, 3],
-            2: [6, 5, 8, 7],
-        },
-    )
+    if sw_tx == 2:
+        adar = adi.adar1000_array(
+            uri=rpi_ip,
+            chip_ids=["BEAM0", "BEAM1"],
+            device_map=[[1], [2]],
+            element_map=[[1, 2, 3, 4], [5, 6, 7, 8]],
+            device_element_map={
+                1: [2, 1, 4, 3],
+                2: [6, 5, 8, 7],
+            },
+        )
+    else:
+        adar = adi.adar1000_array(
+            uri=rpi_ip,
+            chip_ids=["BEAM0"],
+            device_map=[[1]],
+            element_map=[[1, 2, 3, 4]],
+            device_element_map={
+                1: [2, 1, 4, 3]
+            },
+        )
 
     beam_list = []  # List of Beamformers in order to steup and configure Individually.
     for device in adar.devices.values():
@@ -47,6 +58,13 @@ def main():
     for adar in beam_list:
         ADAR_init(adar)  # resets the ADAR1000, then reprograms it to the standard config/ Known state
         ADAR_set_RxTaper(adar)  # Set gain of each channel of all beamformer according to the Cal Values
+
+    cal = input("Do you want to Calibrate the system?")
+    if ("yes" or "Yes") in cal:
+        Phase_calibration(beam_list, sdr)
+        for adar in beam_list:
+            ADAR_init(adar)  # resets the ADAR1000, then reprograms it to the standard config/ Known state
+            ADAR_set_RxTaper(adar)  # Set gain of each channel of all beamformer according to the Cal Values
 
     ADAR_Plotter(beam_list, sdr)  # Rx down-converted signal and plot it to get sinc pattern
 
