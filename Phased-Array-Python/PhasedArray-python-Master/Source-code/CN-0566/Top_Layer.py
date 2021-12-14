86c647fd4c0cfb83b2aaa0717d10f2c97a74a6b5
Darshil Trivedi <darshiltrivedi04@gmail.com>
2021-12-14 11:16:58 -0800

    Comments added and modified imports

--- Phased-Array-Python/PhasedArray-python-Master/Source-code/CN-0566/Top_Layer.py
+++ Phased-Array-Python/PhasedArray-python-Master/Source-code/CN-0566/Top_Layer.py
@@ -1,11 +1,12 @@
-from cn0566 import *
+from CN0566 import sdr, pll, beamformer
 
 rpi_ip = "ip:10.84.10.66"  # IP address of the Raspberry Pi
 lo_ip = "ip:192.168.2.1"  # IP address of the Transreceiver Block
 
-my_pll = pll.adf4159(uri=rpi_ip)
+# Instantiate all the Devices
+my_pll = pll(uri=rpi_ip)
 my_sdr = sdr(uri=lo_ip)
-my_antenna_array = beamformer(
+my_beamformer = beamformer(
     uri=rpi_ip,
     chip_ids=["BEAM0", "BEAM1"],
     device_map=[[1], [2]],
@@ -15,15 +16,39 @@ my_antenna_array = beamformer(
         2: [6, 5, 8, 7],
     },
 )
-
+"""Configure all the device as required in Phased array beamformer project
+    Current freq plan is Sig Freq = 10.492 GHz, antenna element spacing = 0.015in Freq of pll is 12/2 GHz
+    this is mixed down using mixer to get 10.492 - 6 = 4.492GHz which is freq of sdr.
+    This frequency plan can be updated any time in example code
+    e.g:- my_beamformer.SigFreq = 9Ghz etc"""
 my_sdr.configure()
 my_pll.configure()
-my_antenna_array.configure(device_mode="rx")
-my_antenna_array.rx_dev = my_sdr
-# my_antenna_array.set_chan_gain(3, 120)
-# my_antenna_array.load_gain('gain_cal_val.pkl')
-# my_antenna_array.set_all_gain()
-my_antenna_array.plot()
-# my_antenna_array.set_beam_angle(45)
-# my_antenna_array.gain_calibration()
-# my_antenna_array.phase_calibration()
+my_beamformer.configure(device_mode="rx")
+
+""" beamformer/adar1000 uses sdr to plot the output. In order to solve that dependency we can instantiate the 
+    sdr device in beamformer class but that requires pass extra argument i.e. ip of sdr in beamformer as SDR 
+    can be connected to raspberry pi which has all other devices or a different PC that can have diff ip.
+    Alternate option is specify sdr/rx_dev instantiated on top layer like done here"""
+my_beamformer.rx_dev = my_sdr
+
+"""This can be useful in Array size vs beam width experiment or beamtappering experiment. Set the gain of outer channels
+    to 0 and beam width will increase and so on."""
+my_beamformer.set_chan_gain(3, 120)  # set gain of Individual channel
+my_beamformer.set_all_gain()  # Set all gain to calibrated values, if not cal, 127 i.e. max gain
+
+"""If you want to use previously calibrated values load_gain and load_phase values by passing path of previously
+    stored values. If this is not done system will be working as uncalibrated system"""
+my_beamformer.load_gain_cal('gain_cal_val.pkl')
+my_beamformer.load_phase_cal('phase_cal_val.pkl')
+
+
+"""This can be used to change the angle of center lobe i.e if we want to concentrate main lobe/beam at 45 degress"""
+my_beamformer.set_beam_angle(45)
+
+""" Plot the output based on experiment that you are performing"""
+my_beamformer.plot(plot_type="monopulse")
+
+""" This instantiate calibration routine and perform gain and phase calibration. Note gain calibration should be always
+    done 1st as phase calibration depends on gain cal values if not it throws error"""
+my_beamformer.gain_calibration()
+my_beamformer.phase_calibration()
